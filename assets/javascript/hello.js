ec3a3b858a797f89ffa289d68e71b880c7c7cd3f
thaonhu24 <thaonhu1240@gmail.com>
2021-10-16 00:35:26 +0700

    full header

--- assets/javascript/hello.js
+++ assets/javascript/hello.js
@@ -1,14 +1,19 @@
 //xử lý đóng mở menu
-$(document).on('click', ".icon-close, .over", function() {
-    $('.over, .nav').fadeOut(300 , function() {
-        $('.over').remove();
-    });
-    $('.nav').fadeOut();
-    $('.icon-menu').show(100)
-   return false;
-});
-$('.icon-menu').click(function(){
-    $('.nav').show();
-    $('body').append('<div class="over">');
-    $('.over').fadeIn();
+jQuery(document).ready(function($){
+    if($(''))
+        $(document).on('click', ".icon-close, .over", function() {
+            $('.over, .nav').fadeOut(300 , function() {
+                $('.over').remove();
+            });
+            $('.nav').hide();
+            $('.icon-menu').show(100)
+        return false;
+        });
+        $('.icon-menu').click(function(){
+            $('.nav').show();
+            $('body').append('<div class="over">');
+            $('.over').fadeIn();
+        })
+ 
 })
+
