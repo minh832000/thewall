c0b0f55e16f9689042b98549ae3d64ed7b22b5f4
thaonhu24 <thaonhu1240@gmail.com>
2021-10-16 18:43:36 +0700

    review-adv

--- assets/javascript/hello.js
+++ assets/javascript/hello.js
@@ -14,6 +14,38 @@ jQuery(document).ready(function($){
             $('body').append('<div class="over">');
             $('.over').fadeIn();
         })
- 
+ // Slide review 1
+    $('#previous').on('click', function(){
+    // Change to the previous Card
+        $('#card_' + currentCard).stop().fadeOut(5);
+        decreaseCard();
+        $('#card_' + currentCard).stop().fadeIn(5);
+        });
+        $('#next').on('click', function(){
+        // Change to the next Card
+        $('#card_' + currentCard).stop().fadeOut(5);
+        increaseCard();
+        $('#card_' + currentCard).stop().fadeIn(5);
+    });
+    var currentCard = 1;
+    var totalCards = 3;
+    function increaseCard() {
+        ++currentCard;
+        if(currentCard > totalCards) {
+            currentCard = 1;
+        }
+    }
+    function decreaseCard() {
+    /* Decrease currentCard by 1.
+    * Resets to totalCards if smaller than 1
+    */
+        --currentCard;
+        if(currentCard < 1) {
+            currentCard = totalCards;
+        }
+    }
+    window.setInterval(function() {
+        $('#previous').click();
+    }, 5000);
 })
 
